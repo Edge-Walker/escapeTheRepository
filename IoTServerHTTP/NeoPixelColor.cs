15b2e2ed97c1d59dbb424f1754fd9349f719b698
jlgrills <jlgrills@amazon.com>
2020-01-16 17:15:57 -0800

    Added color "aqua" and tweaked magenta and purple

--- IoTServerHTTP/NeoPixelColor.cs
+++ IoTServerHTTP/NeoPixelColor.cs
@@ -1,9 +1,10 @@
 public enum NeoPixelColor {
-    Magenta = 0xCC00AA,
+    Magenta = 0xFC03F0,
     Red = 0xFF0000,
-    Purple = 0x8800FF,
+    Purple = 0x9403FC,
     Blue = 0x0000FF,
     Green = 0x00FF00,
+    Aqua = 0x08FFB9,
     Yellow = 0xFFFF00,
     White = 0xCCCCCC,
     Black = 0x000000
